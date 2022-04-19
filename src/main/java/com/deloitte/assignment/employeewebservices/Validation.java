f6bd09b4d8b98ce07fdf8069a37c627dc1d9f15a
aditya3110 <49290824+aditya3110@users.noreply.github.com>
2022-04-19 23:27:44 +0530

    Add files via upload

---
+++ src/main/java/com/deloitte/assignment/employeewebservices/Validation.java
@@ -0,0 +1,44 @@
+package com.deloitte.assignment.employeewebservices.validation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import com.deloitte.assignment.employeewebservices.Employee;
+
+@Component
+public class Validation{
+
+@Value("${employee.minSalary}")
+private int minSalary;
+
+@Value("${employee.maxLetter}")
+private int maxLetter;
+
+private Employee employee;
+
+
+public Validation() {
+	super();	
+	// TODO Auto-generated constructor stub
+}
+
+
+public void setEmployee(Employee employee) {
+	this.employee = employee;
+}
+
+
+
+public Boolean validateEntry(){
+	
+	if(employee.getEmpName().length() > maxLetter && employee.getSalary() < minSalary)
+		return false;
+	
+	return true;
+	
+}
+
+}
